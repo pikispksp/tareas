a1ecee4de9d0d99f5284d6f61f6361c27419674f
pikispksp <pikispksp@hotmail.com>
2013-02-22 10:40:12 -0500

    Update Square2.java

--- Square2.java
+++ Square2.java
@@ -1,3 +1,8 @@
-class Square2 { public static void printSquare(int x){ System.out.println(x*x); }
-public static void main(String[] arguments){ printSquare("hello"); printSquare(5.5);
-} }
+class Square2 { 
+public static void printSquare(int x){
+System.out.println(x*x); }
+public static void main(String[] arguments){
+printSquare("hello"); 
+printSquare(5.5);
+} 
+}
