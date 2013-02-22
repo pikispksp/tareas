a43d8b4753fe7418987f7d584fd074673ab49429
pikispksp <pikispksp@hotmail.com>
2013-02-22 10:39:28 -0500

    Update Square3.java

--- Square3.java
+++ Square3.java
@@ -1,2 +1,7 @@
-class Square3 { public static void printSquare(double x){ System.out.println(x*x); }
-public static void main(String[] arguments){ printSquare(5); } }
+class Square3 { 
+public static void printSquare(double x){ 
+System.out.println(x*x); }
+public static void main(String[] arguments){ 
+printSquare(5); 
+} 
+}
