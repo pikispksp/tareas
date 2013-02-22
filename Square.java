3aab4e25f0ae326dc6d0eb7a98a03daf3c980d17
pikispksp <pikispksp@hotmail.com>
2013-02-22 10:40:50 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,3 +1,10 @@
-class Square { public static void printSquare(int x){ System.out.println(x*x); }
-public static void main(String[] arguments){ int value = 2; printSquare(value); printSquare(3); printSquare(value*2);
-} }
+class Square { 
+public static void printSquare(int x){ 
+System.out.println(x*x); }
+public static void main(String[] arguments){ 
+int value = 2;
+printSquare(value); 
+printSquare(3); 
+printSquare(value*2);
+} 
+}
